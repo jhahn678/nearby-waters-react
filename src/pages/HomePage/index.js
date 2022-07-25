2d1bb9fb904bb7916e8634ef0424b9302314be0e
jhahn678 <jhahn678@yahoo.com>
2022-07-25 16:26:56 -0400

    Additional input on main page  and updated reducer logic

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,82 +1,96 @@
-import { useState, useReducer, useEffect } from 'react'
+import { useReducer} from 'react'
 import { reducer, initialState } from './reducer'
 import classes from './HomePage.module.css'
 import Page from '../../components/shared/Page'
 import { BiCurrentLocation } from 'react-icons/bi'
-import { Button, TextInput, Select, Text, Modal, Loader } from '@mantine/core'
-import { milesToKM } from '../../utils/milesToKM.ts'
+import { Button, TextInput, Select, Text, Loader } from '@mantine/core'
 import { useGeolocated } from 'react-geolocated'
 import { useGetGeometriesQuery } from '../../hooks/queries/useGetGeometriesQuery.ts'
-import Map from '../../components/map/Map'
+import Map from '../../components/map/Map.tsx'
+import ErrorModal from '../../components/modals/ErrorModal'
+import { LocationErrors } from '../../utils/locationErrors.ts'
 
 const HomePage = () => {
 
   const [state, dispatch] = useReducer(reducer, initialState)
-  const [showErrors, setShowErrors] = useState(false)
-  const [positionLoading, setPositionLoading] = useState(false)
-  const [locationErrorModalOpen , setLocationErrorModalOpen] = useState(false)
 
-  const { data, isLoading, isError, refetch: fetchGeometries } = useGetGeometriesQuery({
+  const { data, isLoading, isError, refetch: fetchGeometries, remove: clearQuery } = useGetGeometriesQuery({
     lat: state?.latitude.value,
-    lon: state?.longitude.value,
+    lng: state?.longitude.value,
     maxdis: state?.distance.value,
+    search: state?.search.value
   })
 
   //data is working
 
   const { isGeolocationAvailable, isGeolocationEnabled, getPosition } = useGeolocated({
     suppressLocationOnMount: true,
-    onSuccess: ({ coords }) => {
-      dispatch({ type: 'COORDS', coords })
-      setPositionLoading(false)
-    },
-    onError: () => setLocationErrorModalOpen(true),
+    onSuccess: ({ coords }) => dispatch({ type: 'POSITION_FOUND', coords }),
+    onError: () => dispatch({ type: 'LOCATION_ERROR', value: LocationErrors.LocationError })
   })
 
   
   const handleCurrentLocation = () => {
-    if(!isGeolocationEnabled || !isGeolocationAvailable){
-      setLocationErrorModalOpen(true)
+    if(!isGeolocationEnabled){
+      dispatch({ type: 'LOCATION_ERROR', value: LocationErrors.LocationServicesDisabled})
+    }else if(!isGeolocationAvailable){
+      dispatch({ type: 'LOCATION_ERROR', value: LocationErrors.LocationServicesUnavailable })
     }else{
-      setPositionLoading(true)
+      dispatch({ type: 'GET_POSITION' })
       getPosition()
     }
   }
 
   const handleSubmit = () => {
-    const { latitude, longitude, distance, unit } = state;
-    if(latitude.valid && longitude.valid && distance.valid){
-      setShowErrors(false)
+    const { latitude, longitude } = state;
+    if(latitude.valid && longitude.valid){
+      dispatch({ type: 'HIDE_ERRORS'})
       fetchGeometries()
     }else{
-      setShowErrors(true)
+      dispatch({ type: 'SHOW_ERRORS' })
     }
+  }
+
+  const handleClear = () => {
+    clearQuery()
+    dispatch({ type: 'RESET' })
   }
 
 
   return (
     <Page className={classes.container}>
+      <ErrorModal visible={state?.currentLocation.showError} 
+        setVisible={() => dispatch({ type: 'HIDE_MODAL'})} 
+        error={state?.currentLocation.error}
+      />
       <div className={classes.sidebox}>
         <div className={classes.form}>
+            <TextInput label='Filter by name' style={{ marginBottom: '2%'}}
+              onInput={e => dispatch({ type: 'SEARCHTERM', value: e.target.value})} 
+              value={state?.search.value} labelProps={{ style: { color: 'whitesmoke' }}}
+              placeholder='Optional'
+            />
             <TextInput label='Latitude' style={{ marginBottom: '2%'}}
               onInput={e => dispatch({ type: 'LAT', value: e.target.value})} 
               value={state?.latitude.value} labelProps={{ style: { color: 'whitesmoke' }}}
-              error={showErrors && !state?.latitude.valid && 'Invalid latitude'}
+              error={state?.form.showErrors && !state?.latitude.valid && 'Invalid latitude'}
             />
             <TextInput label='Longitude'
               value={state?.longitude.value} labelProps={{ style: { color: 'whitesmoke' }}}
               onInput={e => dispatch({ type: 'LON', value: e.target.value})} 
-              error={showErrors && !state?.longitude.valid && 'Invalid longitude'}
+              error={state?.form.showErrors && !state?.longitude.valid && 'Invalid longitude'}
             />
             <Button variant='outline' className={classes.currentLocation} onClick={handleCurrentLocation}>
               <Text weight='500'>Use my current location</Text>
-              { positionLoading ? <Loader variant="dots" size='sm' color='blue' style={{ marginLeft: 8 }}/> : <BiCurrentLocation size={24}/> }
+              { isLoading ? 
+                <Loader variant="dots" size='sm' color='blue' style={{ marginLeft: 8 }}/> : 
+                <BiCurrentLocation size={24}/> 
+              }
             </Button>
           <div className={classes.inputgroup}>
-            <TextInput value={state?.unit.value === 'Kilometers' ? milesToKM(state?.distance.value) : state?.distance.value}
-              onInput={e => dispatch({ type: 'DISTANCE', value: e.target.value })} 
-              label='Search Radius' labelProps={{ style: { color: 'whitesmoke' }}}
-              error={(showErrors && !state?.distance.valid) && 'Invalid distance'}
+            <Select label='Search Radius' data={['10','20','30','40','50']}
+              onChange={val => dispatch({ type: 'DISTANCE', value: val })} 
+              value={state?.distance.value} labelProps={{ style: { color: 'whitesmoke' }}}
             />
             <Select label='Unit of measure' data={['Miles', 'Kilometers']} 
               onChange={val => dispatch({ type: 'UNIT', value: val })} 
@@ -88,12 +102,29 @@ const HomePage = () => {
               fullWidth className={classes.button} 
             >
               <Text>Search</Text>
-              { isLoading && <Loader variant="dots" size='sm' color='black' style={{ marginLeft: 8 }}/> }
+              { state?.currentLocation.isLoading && (
+                <Loader variant="dots" size='sm' color='black' style={{ marginLeft: 8 }}/> 
+              )}
             </Button>
+            <Button variant='outline'  
+              style={{ marginTop: '1em' }} fullWidth
+              onClick={handleClear}
+            >
+              <Text>Clear</Text>
+            </Button>
+
         </div>
       </div>
       <div className={classes.main}>
-        <Map/>
+        <Map data={data}
+          setCoords={coords => dispatch({ type: 'COORDS', value: coords })}
+          query={{ 
+            lat: state?.latitude.value, 
+            lng: state?.longitude.value, 
+            maxdis: state?.distance.value,
+            search: state?.search.value
+          }}
+        />
       </div>
     </Page>
   )
--- src/pages/HomePage/reducer.js
+++ src/pages/HomePage/reducer.js
@@ -1,8 +1,15 @@
 export const initialState = {
+    search: { value: '' },
     latitude: { value: '', valid: false },
     longitude: { value: '', valid: false },
-    distance: { value: 10, valid: true },
-    unit: { value: 'Miles' }
+    distance: { value: '10' },
+    unit: { value: 'Miles' },
+    form: { showErrors: false, isLoading: false },
+    currentLocation: { 
+        isLoading: false, 
+        showError: false,
+        error: null
+    } 
 }
 
 export const reducer = (state, action) => {
@@ -25,22 +32,67 @@ export const reducer = (state, action) => {
         )
         return { ...state, longitude }
     }else if(action.type === 'COORDS'){
-        const { latitude, longitude} = state;
-        latitude.value = action.coords.latitude;
+        const { latitude, longitude } = state;
+        const { lng, lat } = action.value;
+        latitude.value = lat.toFixed(6);
         latitude.valid = true;
-        longitude.value = action.coords.longitude;
+        longitude.value = lng.toFixed(6);
         longitude.valid = true;
         return { ...state, latitude, longitude }
     }else if(action.type === 'DISTANCE'){
         const { distance } = state;
         distance.value = action.value;
-        distance.valid = parseInt(action.value);
         return { ...state, distance }
     }else if(action.type === 'UNIT'){
         const { unit } = state;
         unit.value = action.value;
         return { ...state, unit }
+    }else if(action.type === 'SHOW_ERRORS'){
+        const { form } = state;
+        form.showErrors = true;
+        return { ...state, form }
+    }else if(action.type === 'HIDE_ERRORS'){
+        const { form } = state;
+        form.showErrors = false;
+        return { ...state, form }
+    }else if(action.type === 'GET_POSITION'){
+        const { currentLocation } = state;
+        currentLocation.isLoading = true;
+        return { ...state, currentLocation }
+    }else if(action.type === 'POSITION_FOUND'){
+        const { currentLocation, latitude, longitude } = state;
+        currentLocation.isLoading = false;
+        currentLocation.showError = false;
+        currentLocation.error = null;
+        latitude.value = action.coords.latitude.toFixed(6);
+        latitude.valid = true;
+        longitude.value = action.coords.longitude.toFixed(6);
+        longitude.valid = true;
+        return { ...state, latitude, longitude, currentLocation }
+    }else if(action.type === 'LOCATION_ERROR'){
+        const { currentLocation } = state;
+        currentLocation.error = action.value;
+        currentLocation.showError = true;
+        currentLocation.isLoading = false
+        return { ...state, currentLocation }
+    }else if(action.type === 'HIDE_MODAL'){
+        const { currentLocation } = state;
+        currentLocation.showError = false;
+        currentLocation.error = null;
+        return { ...state, currentLocation }
+    }else if(action.type === 'SEARCHTERM'){
+        const { search } = state;
+        search.value = action.value;
+        return { ...state, search }
+    }else if(action.type === 'RESET'){
+        const { search, latitude, longitude } = state;
+        search.value = '';
+        latitude.value = '';
+        latitude.valid = false;
+        longitude.value = '';
+        longitude.valid = false;
+        return { ...state, search, latitude, longitude }
     }else{
-        return {...initialState}
+        return { ...initialState };
     }
 } 
