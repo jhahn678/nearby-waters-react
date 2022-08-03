fa8e5fa0d96da6e9d6cb771a8893194d963ea187
jhahn678 <jhahn678@yahoo.com>
2022-08-03 13:16:04 -0400

    react-query defaults

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import { MapProvider } from 'react-map-gl';
 import ErrorModal from './components/modals/ErrorModal/ErrorModal';
 import ModalContextProvider from './hooks/contexts/modal/ModalContextProvider';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({ 
+  defaultOptions: { 
+    queries: { 
+      refetchOnMount: false, 
+      refetchOnWindowFocus: false, 
+      refetchOnReconnect: false
+    } 
+  } 
+})
 
 const App = (): JSX.Element => {
     return (
