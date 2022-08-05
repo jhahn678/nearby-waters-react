ab2bb2213a23bd21c30928ac27e1453d5c408aa4
jhahn678 <jhahn678@yahoo.com>
2022-08-04 23:05:03 -0400

    Fetches single waterbody to be displayed on map

--- src/hooks/queries/useGetWaterbody.ts
+++ src/hooks/queries/useGetWaterbody.ts
@@ -1,11 +1,9 @@
 import { useQuery } from 'react-query'
+import { PopulatedWaterbody } from '../../types/Waterbody'
 import axios from '../../utils/axios'
 
-interface Waterbody {
 
-}
-
-const getWaterbody = async (_id: string | null): Promise<Waterbody> => {
+const getWaterbody = async (_id: string | null): Promise<PopulatedWaterbody> => {
     if(!_id) Promise.reject('_id not valid')
     const result = await axios.get(`/waterbody?_id=${_id}&geometries=true`)
     return result.data
@@ -13,7 +11,7 @@ const getWaterbody = async (_id: string | null): Promise<Waterbody> => {
 
 export const useGetWaterbody = (_id: string | null) => {
 
-    const result = useQuery<Waterbody, Error>({
+    const result = useQuery<PopulatedWaterbody, Error>({
         queryKey: `waterbody-${_id}`,
         queryFn: () => getWaterbody(_id),
         enabled: false
