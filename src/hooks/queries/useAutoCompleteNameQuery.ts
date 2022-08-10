178393cc0f53b0bdf9243e52d00d6649656a3e41
jhahn678 <jhahn678@yahoo.com>
2022-08-10 15:34:07 -0400

    Queries for names of waterbodies that appear multiple times

--- src/hooks/queries/useAutoCompleteNameQuery.ts
+++ src/hooks/queries/useAutoCompleteNameQuery.ts
@@ -1,8 +1,9 @@
+import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import axios from '../../utils/axios'
 
 const getDistinctNames = async (value: string): Promise<string[]> => {
-    const res = await axios.get(`/autocomplete/waterbodies/distinct-name?value=${value}`)
+    const res = await axios.get(`/autocomplete/waterbodies/distinct-duplicated-name?value=${value}`)
     return res.data
 }
 
@@ -14,9 +15,18 @@ interface DistinctNameQuery {
 export const useAutoCompleteNameQuery = ({ 
     value, shouldQuery
 }: DistinctNameQuery) => {
+
     const result = useQuery<string[], Error>({
         queryKey: 'autocomplete-distinct-names',
         queryFn: () => getDistinctNames(value),
-        enabled: shouldQuery
+        enabled: shouldQuery && value.length >= 6
     })
+
+    useEffect(() => {
+        if(value.length < 4 && result.data){
+            result.remove()
+        }
+    },[value])
+
+    return result;
 }
