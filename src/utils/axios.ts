5a51e73302cafcde3a0a033b99e2d101c224c990
jhahn678 <jhahn678@yahoo.com>
2022-08-10 15:25:49 -0400

    Added auth headers to config

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,18 @@
-import Axios from 'axios'
+import Axios, { AxiosRequestConfig } from 'axios'
+
+const config: AxiosRequestConfig = {
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+}
+
+const token = localStorage.getItem('AUTH_TOKEN')
+
+if(token) {
+    config.headers = {
+        'Authorization': `Bearer ${token}`
+    }
+}
 
 
-const axios = Axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL
-})
+const axios = Axios.create(config)
 
 export default axios;
